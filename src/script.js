5c75843ca2eab6921c1aa7bdd3f18b58b0bea069
CursedMushroom <osl2877@rit.edu>
2024-07-06 17:08:16 -0400

    Removed Old Custom Drop Down, Added expected inputs and added site wide nav bar
    removed old custom drop down, added expected search variables (number, 2 drop downs , 2 buttons), and added full responsive nav bar that will be used throughout site.

--- src/script.js
+++ src/script.js
@@ -54,21 +54,10 @@ async function fetchJSON(url) {
 fetchJSON('json/allRaces.json');
 
 
-// script.js
-var tree = document.querySelectorAll('ul.tree a:not(:last-child)');
-for(var i = 0; i < tree.length; i++){
-    tree[i].addEventListener('click', function(e) {
-        var parent = e.target.parentElement;
-        var classList = parent.classList;
-        if(classList.contains("open")) {
-            classList.remove('open');
-            var opensubs = parent.querySelectorAll(':scope .open');
-            for(var i = 0; i < opensubs.length; i++){
-                opensubs[i].classList.remove('open');
-            }
-        } else {
-            classList.add('open');
-        }
-        e.preventDefault();
-    });
-}
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
